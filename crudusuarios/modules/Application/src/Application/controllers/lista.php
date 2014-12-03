465970f555961c769c00ff702130d5c98415df3a
patriR <probles.alf@gmail.com>
2014-12-03 11:08:53 +0100

    lista

---
+++ crudusuarios/modules/Application/src/Application/controllers/lista.php
@@ -0,0 +1,5 @@
+<?php
+//Obtener la lista de controladores del directorio controllers
+$validControllers = array ('users', 'error');
+$validActions = array( 
+    'users' => array ('insert', 'update', 'delete', 'select'));
