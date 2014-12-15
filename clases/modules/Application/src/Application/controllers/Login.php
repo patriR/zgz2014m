cf2356cb02b4555bd3645f0fc14260d1306b55a2
patriR <probles.alf@gmail.com>
2014-12-15 11:15:32 +0100

    Cambios clases

---
+++ clases/modules/Application/src/Application/controllers/Login.php
@@ -0,0 +1,55 @@
+<?php
+namespace Application\controllers;
+use \Core\Application;
+class Login
+{
+    public $layout = 'index.phtml';
+    
+    public function logout()
+    {
+        unset($_SESSION['email']);
+        session_regenerate_id();
+        header("Location: /Home/select");
+        exit();
+    }
+    
+    public function index()
+    {
+        if($_POST)
+        {
+            // Conectarse al DBMS
+            
+            $link = mysqli_connect(Application::getConfig()['database']['host'],
+                Application::getConfig()['database']['user'],
+                Application::getConfig()['database']['password']);
+            // Seleccionar la DB
+            mysqli_select_db($link, Application::getConfig()['database']['database']);
+             
+            // SELECT * FROM users WHERE id;
+        
+            $sql = "SELECT iduser, name, email FROM users
+                WHERE password = '".$_POST['password']."'
+                AND email ='".$_POST['email']."'";
+             
+            // Retornar el data
+            $result = mysqli_query($link, $sql);
+            echo mysqli_num_rows($result);
+            if(mysqli_num_rows($result)===1)
+            {
+                session_regenerate_id();
+                $_SESSION['email']=$_POST['email'];
+                 
+                header("Location: /Users/select");
+            }
+            else
+            {
+                header("Location: /Login/index");
+            }
+        }
+        else
+        {
+            //include('../modules/Application/src/Application/layouts/index.phtml');
+        }
+        
+    }
+}
